b142d7461be24b96bb554fb1d8ccbfe1ddbc9079
lilpumpy19 <100938542+lilpumpy19@users.noreply.github.com>
2024-05-19 15:08:15 +0400

    not working

--- src/main/java/com/example/gradebook/config/AppConfig.java
+++ src/main/java/com/example/gradebook/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.gradebook.config;
 
+import com.example.gradebook.dto.GroupDTO;
 import com.example.gradebook.dto.StudentDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,5 +10,10 @@ public class AppConfig {
     @Bean
     public StudentDTO studentDto() {
         return new StudentDTO();
+    }
+
+    @Bean
+    public GroupDTO groupDto() {
+        return new GroupDTO();
     }
 }
--- src/main/java/com/example/gradebook/kafka/StudentKafkaListener.java
+++ src/main/java/com/example/gradebook/kafka/StudentKafkaListener.java
@@ -1,20 +1,20 @@
-//package com.example.gradebook.kafka;
-//
-//
-//import com.example.gradebook.dto.StudentDTO;
-//import com.example.gradebook.service.StudentService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class StudentKafkaListener {
-//    private final StudentService studentService;
-//
-//    @KafkaListener(topics = {"students"})
-//    public void studentsListener(StudentDTO studentDTO) {
-//        studentService.addStudent(studentDTO);
-//    }
-//
-//}
+package com.example.gradebook.kafka;
+
+
+import com.example.gradebook.dto.StudentDTO;
+import com.example.gradebook.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StudentKafkaListener {
+    private final StudentService studentService;
+
+    @KafkaListener(topics = {"students"})
+    public void studentsListener(StudentDTO studentDTO) {
+        studentService.addStudent(studentDTO);
+    }
+
+}
